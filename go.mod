330926082e237e91ae437654ae5090c47a0a8543
congle-engineer <congle-engineer@gmail.com>
2026-01-19 16:38:12 +0700

    Update

--- go.mod
+++ go.mod
@@ -6,5 +6,9 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.4.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.47.0 // indirect
 )
---
+++ src/goweb/json.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type User struct {
+	FirstName string `json:"firstname"`
+	LastName string `json:"lastname"`
+	Age int `json:"age"`
+}
+
+func main() {
+	http.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request) {
+		var user User
+		json.NewDecoder(r.Body).Decode(&user)
+
+		fmt.Fprintf(w, "%s %s is %d years old!", user.FirstName, user.LastName, user.Age)
+	})
+
+	http.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request) {
+		congle := User {
+			FirstName: "Cong",
+			LastName: "Le",
+			Age: 33,
+		}
+
+		json.NewEncoder(w).Encode(congle)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
